da3d7b6facf11183a75a119e7dd1ef551cb05a23
john_lee <jjlee099@gmail.com>
2018-02-13 12:56:40 -0800

    adds itemCard

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import itemCard from '../containers/itemCard'
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>React simple starter
+      </div>
     );
   }
 }
